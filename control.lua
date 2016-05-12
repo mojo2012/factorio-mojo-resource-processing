601420408fefb3acf3d1359332dd5df4c20f98d6
Matthias Fuchs <matthias.fuchs@getit.de>
2016-05-12 13:18:54 +0200

    added some new default files and folders (empty) and added some comments

--- control.lua
+++ control.lua
@@ -1,3 +1,7 @@
+-- initialization and event handling during the game
+
 require("logic.helper")
+
+-- specific event handling for the various sub parts of the mod
 require("logic.ore-processing.control")
 require("logic.no-hand-crafting.control")
--- data.lua
+++ data.lua
@@ -1,10 +1,14 @@
+-- needed during factorio startup to initialize the plugin
+
+-- some helper functions necessary to deal some internal initialization
 require("logic.helper")
 
+-- the actual modded game items
 require("prototypes.item.base")
 require("prototypes.entity.base")
 require("prototypes.recipe.base")
 require("prototypes.technology.base")
 
-
+-- some init stuff specific to the various sub parts of the mod
 require("logic.ore-processing.init")
 require("logic.no-hand-crafting.init")
